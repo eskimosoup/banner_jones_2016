aae8f42d7b08d5b04bf076c62588b5b009f49ecd
Paul <pauloptimised@users.noreply.github.com>
2016-03-21 16:10:57 +0000

    Begin to add in display statuses to module indexes

---
+++ app/controllers/optimadmin/article_categories/published_controller.rb
@@ -0,0 +1,18 @@
+module Optimadmin
+  module ArticleCategories
+    class PublishedController < Optimadmin::ArticleCategoriesController
+      layout 'optimadmin/inline_editing'
+
+      def index
+        @article_categories = Optimadmin::BaseCollectionPresenter.new(
+          collection: ArticleCategory.published
+                        .where('title ILIKE ?', "%#{params[:search]}%")
+                        .page(params[:page]).per(params[:per_page] || 1),
+          view_template: view_context,
+          presenter: Optimadmin::ArticleCategoryPresenter
+        )
+        render 'optimadmin/article_categories/ajax/index'
+      end
+    end
+  end
+end
--- app/models/concerns/display_status.rb
+++ app/models/concerns/display_status.rb
@@ -4,13 +4,13 @@ module DisplayStatus
   included do
     enum status: { published: 1, scheduled: 2, expired: 3 }
     validates :publish_at, presence: true, if: proc { |x| x.has_attribute?('publish_at') }
-    before_save :manage_status, if: proc { |x| x.has_attribute?('publish_at') }
+    after_validation :manage_status, if: proc { |x| x.has_attribute?('publish_at') }
   end
 
   def manage_status
-    self.status = if expire_at.present? && expire_at <= Time.zone.now
+    self.status = if expire_at <= Time.zone.now
                     2
-                  elsif publish_at.present? && publish_at <= Time.zone.now
+                  elsif publish_at <= Time.zone.now
                     1
                   else
                     3
