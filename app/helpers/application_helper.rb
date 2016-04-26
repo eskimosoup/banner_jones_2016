0697cb442ac66304d9f3ba08ff6f4d7f72dc3c89
Paul <pauloptimised@users.noreply.github.com>
2016-04-26 16:12:48 +0100

    Responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
 
   def social_icon(type)
     return if global_site_settings["#{type} URL"].blank?
-    link_to global_site_settings["#{type} URL"], title: type, class: "social-icon social-#{}" do
+    link_to global_site_settings["#{type} URL"], title: type, class: "social-icon social-#{type.parameterize.dasherize}" do
       image_tag "components/icons/social/#{type.downcase.parameterize}.png", alt: type
     end
   end
