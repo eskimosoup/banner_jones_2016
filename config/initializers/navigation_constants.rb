f740f55b45278a5058ae989d2dd5168872577f14
Paul <pauloptimised@users.noreply.github.com>
2016-05-03 09:49:22 +0100

    Add page to navigation

--- config/initializers/navigation_constants.rb
+++ config/initializers/navigation_constants.rb
@@ -3,5 +3,6 @@ NavigationMenus = %w( header_aside primary_header footer ).freeze
 NavigationLinks = {
   'Module Page' => 'Optimadmin::ModulePage',
   'External Link' => 'Optimadmin::ExternalLink',
-  'Audience' => 'Audience'
+  'Audience' => 'Audience',
+  'Page' => 'Page'
 }.freeze
