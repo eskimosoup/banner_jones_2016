2c3f4a73d82417fdcc5426005b96c3116111b06d
Paul <pauloptimised@users.noreply.github.com>
2016-05-20 12:28:51 +0100

    Testimonial and FAQ content

--- app/controllers/frequently_asked_questions_controller.rb
+++ app/controllers/frequently_asked_questions_controller.rb
@@ -1,6 +1,7 @@
 class FrequentlyAskedQuestionsController < ApplicationController
   def index
-    @services = Service.displayed.root_services.order(title: :asc)
+    @service_ids = Services::Testimonial.select(:service_id).distinct.pluck(:service_id)
+    @audiences = Audience.displayed
     @service = Service.find(params[:service_id]) if params[:service_id]
     @faqs = @service.present? ? @service.frequently_asked_questions.displayed.page(params[:page]).per(params[:per_page] || 25) : FrequentlyAskedQuestion.displayed.page(params[:page]).per(params[:per_page] || 25).limit(100)
   end
