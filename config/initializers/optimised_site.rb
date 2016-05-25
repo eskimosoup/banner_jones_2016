9b06a827aed1a95a36c706cb41e3a3ea36dd018f
James Doyley <jdoyley@gmail.com>
2016-05-25 11:52:52 +0100

    Add service colours

---
+++ config/initializers/optimised_site.rb
@@ -0,0 +1,13 @@
+OptimisedSite.configure do |config|
+  config.service_colours = [
+    ["Blue", "#0078ae"],
+    ["Dark Pink", "#891b57"],
+    ["Green", "#40ae49"],
+    ["Grey", "#a7a9ac"],
+    ["Lime Green", "#8dc63f"],
+    ["Orange", "#fdba31"],
+    ["Pink", "#d11241"],
+    ["Purple", "#522f91"],
+    ["Turquoise", "#00a1b1"],
+  ]
+end
