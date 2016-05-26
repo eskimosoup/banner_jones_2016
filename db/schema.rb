5c35fb4a7c8a10e81560928f2ebb9b5aa6ddcf91
Paul <pauloptimised@users.noreply.github.com>
2016-05-26 12:19:20 +0100

    Amends

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518143126) do
+ActiveRecord::Schema.define(version: 20160526082906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(version: 20160518143126) do
   end
 
   create_table "audiences_banners", force: :cascade do |t|
-    t.integer  "audience_id",             null: false
-    t.integer  "banner_id",               null: false
+    t.integer  "audience_id"
+    t.integer  "banner_id"
     t.integer  "position",    default: 0, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
