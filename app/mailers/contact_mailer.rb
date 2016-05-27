64146d54b6f416b09ffd49c167709b7a273dd241
Paul <pauloptimised@users.noreply.github.com>
2016-05-27 15:35:29 +0100

    Fix contact subject

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def new_contact(contact)
     @contact = contact
     mail to: (@contact.wealth_management.present? ? wealth_management_email : site_email),
-         subject: "Callback Request Form Completed #{site_name}"
+         subject: "Contact Form Completed #{site_name}"
   end
 end
