2ea059b76330c17d138834a79be5b73963b47129
Paul <pauloptimised@users.noreply.github.com>
2016-05-27 12:16:24 +0100

    Remove repetition and fix callback for solicitors

--- app/models/concerns/extensions/contact.rb
+++ app/models/concerns/extensions/contact.rb
@@ -16,6 +16,19 @@ module Extensions
       validates :telephone, telephone: true, presence: true, if: proc { |x| x.preferred_contact_method == 'Phone' }
 
       # validate :email_or_telephone
+
+      HOW_HEARDS = ['Google search',
+                    'Yell.com search',
+                    'Other search',
+                    'Advertisement',
+                    'Facebook advert',
+                    'Recommendation',
+                    'Existing client',
+                    'Peak FM advert',
+                    'TV advert'].freeze
+
+      ENQUIRY_TYPES = ['General'].freeze
+      CONTACT_METHODS = %w(Phone Email).freeze
     end
 
     def email_or_telephone
