58e48013792ba11bc832928c1e23a4f971548621
Paul <pauloptimised@users.noreply.github.com>
2016-07-22 16:43:33 +0100

    Add in basic mailchimp prerequisites

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,11 +13,20 @@ class ContactsController < ApplicationController
   private
 
   def process_contact_request(contact)
-    ContactMailer.new_contact(@contact).deliver_now if contact.valid?
+    if contact.valid?
+      ContactMailer.new_contact(@contact).deliver_now
+      subscribe_to_mailchimp(contact)
+    end
     respond_to do |format|
       format.html { html_contact(contact) }
       format.js { js_contact(contact) }
     end
+  end
+
+  def subscribe_to_mailchimp(contact)
+    mailchimp_list = Service.find_by(title: contact.service).mailchimp_list if contact.service.present?
+    mailchimp_list = MailchimpList.find_by(module_id: nil, module_type: nil) if contact.service.blank? || mailchimp_list.blank?
+    Mailchimp.subscribe(mailchimp_list.mailchimp_list_id, contact) if mailchimp_list.present? && mailchimp_list.mailchimp_list_id.present?
   end
 
   def html_contact(contact)
