dbdcb0685484b8334bb6c71b5349ea90a6c1c6b3
Paul <pauloptimised@users.noreply.github.com>
2016-07-25 10:15:24 +0100

    Fix if statement

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,7 +25,7 @@ class ContactsController < ApplicationController
 
   def subscribe_to_mailchimp(contact)
     service = Service.find_by(title: contact.service) if contact.service.present?
-    service = Service.find_by(title: service.root) if service.mailchimp_list.blank?
+    service = Service.find_by(title: service.root) if service.present? && service.mailchimp_list.blank?
     mailchimp_list = (service.present? && service.mailchimp_list.present? ? service.mailchimp_list : MailchimpList.find_by(module_id: nil, module_type: nil))
     Mailchimp.subscribe(mailchimp_list.mailchimp_list_id, contact) if mailchimp_list.present? && mailchimp_list.mailchimp_list_id.present?
   end
