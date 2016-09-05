d3ff1d310f06cca80018ca310a2e7a0027716330
optimised <noreply@optimised.today>
2016-09-05 17:00:28 +0100

    Add service to navigation constants

--- config/initializers/navigation_constants.rb
+++ config/initializers/navigation_constants.rb
@@ -4,6 +4,7 @@ NavigationLinks = {
   'Module Page' => 'Optimadmin::ModulePage',
   'External Link' => 'Optimadmin::ExternalLink',
   'Audience' => 'Audience',
+  'Service' => 'Service',
   'Office' => 'Office',
   'Page' => 'Page'
 }.freeze
