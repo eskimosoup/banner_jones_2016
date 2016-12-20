c808574bcf72acb06fad26874ed2cfe565332c88
optimised <noreply@optimised.today>
2016-12-20 16:35:55 +0000

    Amend site title

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   config.web_console.whitelisted_ips = '192.168.0.0/16'
   config.action_mailer.delivery_method = :smtp
