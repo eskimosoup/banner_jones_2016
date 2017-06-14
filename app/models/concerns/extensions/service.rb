4c96efa55e6eb039fc6e0952d0f09a9f044082da
Paul <pauloptimised@users.noreply.github.com>
2017-06-14 16:22:12 +0100

    Amend colours

--- app/models/concerns/extensions/service.rb
+++ app/models/concerns/extensions/service.rb
@@ -6,6 +6,10 @@ module Extensions
       RELATIONSHIPS = %w(related_services articles case_studies resources events videos frequently_asked_questions offices team_members testimonials).freeze
       LAYOUTS = %w(application wealth_management notary).freeze
       STYLES  = %w(basic purchase_plus).freeze
+
+      def self.colours
+        OptimisedSite.configuration.service_colours
+      end
     end
 
     class_methods do
