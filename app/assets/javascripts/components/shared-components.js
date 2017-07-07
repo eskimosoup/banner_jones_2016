ae9f5e04e1e41c730d540dd1e1bd21ec7d08ec4a
Paul <pauloptimised@users.noreply.github.com>
2017-07-07 10:13:52 +0100

    Amend conveyancing styles and add in extra costs

--- app/assets/javascripts/components/shared-components.js
+++ app/assets/javascripts/components/shared-components.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   teamMembersSlick();
 });
+
+$(document).on('click', '.print-page', function() {
+  window.print();
+  return false;
+})
--- config/initializers/conveyancing_calculator.rb
+++ config/initializers/conveyancing_calculator.rb
@@ -41,6 +41,7 @@ ConveyancingCalculator.configure do |config|
       { lower_bound: 700_001, fee: 750.00 }
     ] }
 
+  # Used within the models
   config.additional_costs = { outside_london: [
     { shared_ownership_scheme: 150 },
     { leasehold_house: 50 },
@@ -56,5 +57,13 @@ ConveyancingCalculator.configure do |config|
       { help_to_buy_scheme: 150 },
       { help_to_buy_isa: 50 },
       { deed_of_trust: 150 }
+    ] }
+
+  # Static in the code
+  config.additional_static_costs = { outside_london: [
+    { premium_search: 270.83 }
+  ], london:
+    [
+      { premium_search: 270.83 }
     ] }
 end
