de09cd808787b733a24d59f6e7268cb2668864ce
optimised <noreply@optimised.today>
2017-07-31 11:58:30 +0100

    Prepare SMTP options

--- app/mailers/conveyancing_quote_mailer.rb
+++ app/mailers/conveyancing_quote_mailer.rb
@@ -3,8 +3,19 @@ class ConveyancingQuoteMailer < ApplicationMailer
   layout false, only: 'new_quote'
   def new_quote(user)
     @user = user
+
+    # delivery_options = {
+    #   user_name: ENV['EMAIL_USERNAME'],
+    #   password: ENV['EMAIL_PASSWORD'],
+    #   address: ENV['EMAIL_HOST'],
+    #   authentication: :login,
+    #   port: 587,
+    #   enable_starttls_auto: true
+    # }
+
     mail to: conveyancing_email,
-         subject: "Conveyancing Quote Completed #{site_name}"
+         subject: "Conveyancing Quote Completed #{site_name}" # ,
+         # delivery_method_options: delivery_options
   end
 
   def incomplete_users(users)
