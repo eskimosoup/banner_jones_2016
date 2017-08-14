f65fee6abac7081396a46056311c6e1ac3a4c48b
optimised <noreply@optimised.today>
2017-08-14 11:20:59 +0100

    Amend mailer

--- app/mailers/conveyancing_quote_mailer.rb
+++ app/mailers/conveyancing_quote_mailer.rb
@@ -1,7 +1,7 @@
 class ConveyancingQuoteMailer < ApplicationMailer
   layout false, only: 'new_quote'
 
-  # ConveyancingQuoteMailer.new_quote(ConveyancingQuotes::User.first).deliver_now
+  # ConveyancingQuoteMailer.new_quote(ConveyancingQuotes::User.where(submitted: true).first).deliver_now
   def new_quote(user)
     @user = user
 
@@ -9,8 +9,7 @@ class ConveyancingQuoteMailer < ApplicationMailer
       address: ENV['EMAIL_HOST']
     }
 
-    # to: conveyancing_email,
-    mail to: 'paul@optimised.today',
+    mail to: conveyancing_email,
          from: 'info@bannerjones.co.uk',
          subject: "Conveyancing Quote Completed #{site_name}",
          delivery_method_options: delivery_options
