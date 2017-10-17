ea5cd56ddbb42aa2eeb02b9f5d9af3296f9a359d
optimised <noreply@optimised.today>
2017-10-17 14:03:06 +0100

    Add info details

--- spec/mailers/previews/conveyancing_quote_mailer_preview.rb
+++ spec/mailers/previews/conveyancing_quote_mailer_preview.rb
@@ -2,4 +2,8 @@ class ConveyancingQuoteMailerPreview < ActionMailer::Preview
   def new_quote
     ConveyancingQuoteMailer.new_quote(ConveyancingQuotes::User.where(submitted: true).last)
   end
+
+  def new_quote_notification
+    ConveyancingQuoteMailer.new_quote_notification(ConveyancingQuotes::User.where(submitted: true).last)
+  end
 end
