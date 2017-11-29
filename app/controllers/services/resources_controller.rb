da95e9260de395d48446357af7a4a4ff230b413e
optimised <noreply@optimised.today>
2017-11-29 10:31:42 +0000

    Resource SEO

--- app/controllers/services/resources_controller.rb
+++ app/controllers/services/resources_controller.rb
@@ -1,9 +1,11 @@
 module Services
   class ResourcesController < ApplicationController
     before_action :find_service
+    skip_before_action :set_seo_variables
 
     def show
       @resource = @service.resources.displayed.find(params[:id])
+      set_seo_variables(@resource)
       @offices = Office.unscoped.displayed.joins(:office_location).order('office_locations.name ASC')
       @contact = Contact.new if @service.landing_page?
       @offices = nil if @service.hide_preferred_office_on_forms?
@@ -14,6 +16,15 @@ module Services
 
     def find_service
       @service = Service.displayed.find(params[:service_id])
+    end
+
+    def set_seo_variables(resource)
+      seo_entry = SeoEntry.find_by(nominal_url: resource_path(resource))
+      return unless seo_entry
+      @rich_snippet = seo_entry.rich_snippet
+      @title = seo_entry.title
+      @meta_description = seo_entry.meta_description
+      @meta_tags = seo_entry.title
     end
   end
 end
