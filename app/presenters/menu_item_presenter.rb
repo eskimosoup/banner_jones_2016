f6a246a503af1056be223825f9b2df0a52e92fdb
optimised <noreply@optimised.today>
2017-12-05 14:41:03 +0000

    Home page redesign progress

--- app/presenters/menu_item_presenter.rb
+++ app/presenters/menu_item_presenter.rb
@@ -20,7 +20,7 @@ class MenuItemPresenter < BasePresenter
 
   def special_layout
     if audience?
-      h.render 'menu_items/meganav', audience: destination
+      # h.render 'menu_items/meganav', audience: destination
     end
     # elsif destination.present? && menu_item.menu_name == 'primary_header' && menu_item.link.menu_resource_type == 'Optimadmin::ModulePage' && menu_item.link.menu_resource.route == 'new_contact_path'
     #  h.render partial: 'menu_items/offices',
