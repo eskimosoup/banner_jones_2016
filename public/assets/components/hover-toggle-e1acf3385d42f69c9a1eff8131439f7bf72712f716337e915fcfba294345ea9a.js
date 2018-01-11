4541aefe361bfbac90f4ab70b2ec3b5a1dec88b7
optimised <noreply@optimised.today>
2018-01-11 14:32:51 +0000

    Clickable to the article pages

--- public/assets/components/hover-toggle-e1acf3385d42f69c9a1eff8131439f7bf72712f716337e915fcfba294345ea9a.js
+++
@@ -1,7 +0,0 @@
-$(document).on('mouseover', '.hover-toggle', function() {
-  var contentSection = $(this).data('container');
-  $(this).parent().parent().find('.active').removeClass('active');
-  $(this).addClass('active');
-  $('.' + contentSection).siblings().addClass('hide');
-  $('.' + contentSection).removeClass('hide');
-});
