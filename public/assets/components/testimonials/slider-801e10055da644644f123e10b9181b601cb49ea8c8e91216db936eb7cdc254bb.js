3252df62ca6d2f1118ec60a4efcd9a0204ab636b
optimised <noreply@optimised.today>
2018-01-19 12:09:48 +0000

    small changes

--- public/assets/components/testimonials/slider-801e10055da644644f123e10b9181b601cb49ea8c8e91216db936eb7cdc254bb.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.testimonials-slick, .service-testimonials').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 6000
-  });
-});
