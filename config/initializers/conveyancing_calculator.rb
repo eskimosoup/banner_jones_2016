e11f2daa5690eeb16ed2c783b3162859c59167ca
optimised <noreply@optimised.today>
2018-03-07 16:08:18 +0000

    Update conveyancing layout

--- config/initializers/conveyancing_calculator.rb
+++ config/initializers/conveyancing_calculator.rb
@@ -1,14 +1,16 @@
 ConveyancingCalculator.configure do |config|
-  config.purchase_fee_bands = { outside_london: [
-    { lower_bound: 0, upper_bound: 100_000, fee: 450.00 },
-    { lower_bound: 100_001, upper_bound: 200_000, fee: 475.00 },
-    { lower_bound: 200_001, upper_bound: 300_000, fee: 525.00 },
-    { lower_bound: 300_001, upper_bound: 400_000, fee: 575.00 },
-    { lower_bound: 400_001, upper_bound: 500_000, fee: 625.00 },
-    { lower_bound: 500_001, upper_bound: 600_000, fee: 700.00 },
-    { lower_bound: 600_001, upper_bound: 700_000, fee: 750.00 },
-    { lower_bound: 700_001, fee: 800.00 }
-  ], london:
+  config.purchase_fee_bands = {
+    outside_london: [
+      { lower_bound: 0, upper_bound: 100_000, fee: 450.00 },
+      { lower_bound: 100_001, upper_bound: 200_000, fee: 475.00 },
+      { lower_bound: 200_001, upper_bound: 300_000, fee: 525.00 },
+      { lower_bound: 300_001, upper_bound: 400_000, fee: 575.00 },
+      { lower_bound: 400_001, upper_bound: 500_000, fee: 625.00 },
+      { lower_bound: 500_001, upper_bound: 600_000, fee: 700.00 },
+      { lower_bound: 600_001, upper_bound: 700_000, fee: 750.00 },
+      { lower_bound: 700_001, fee: 800.00 }
+    ],
+    london:
     [
       { lower_bound: 0, upper_bound: 100_000, fee: 700.00 },
       { lower_bound: 100_001, upper_bound: 200_000, fee: 725.00 },
@@ -23,18 +25,21 @@ ConveyancingCalculator.configure do |config|
       { lower_bound: 150_000_0, upper_bound: 200_000_0, fee: 1550.00 },
       { lower_bound: 200_000_1, upper_bound: 250_000_0, fee: 1700.00 },
       { lower_bound: 250_000_0, fee: 1700.00 }
-    ] }
+    ]
+  }
 
-  config.sale_fee_bands = { outside_london: [
-    { lower_bound: 0, upper_bound: 100_000, fee: 425.00 },
-    { lower_bound: 100_001, upper_bound: 200_000, fee: 450.00 },
-    { lower_bound: 200_001, upper_bound: 300_000, fee: 475.00 },
-    { lower_bound: 300_001, upper_bound: 400_000, fee: 525.00 },
-    { lower_bound: 400_001, upper_bound: 500_000, fee: 575.00 },
-    { lower_bound: 500_001, upper_bound: 600_000, fee: 625.00 },
-    { lower_bound: 600_001, upper_bound: 700_000, fee: 700.00 },
-    { lower_bound: 700_001, fee: 750.00 }
-  ], london:
+  config.sale_fee_bands = {
+    outside_london: [
+      { lower_bound: 0, upper_bound: 100_000, fee: 425.00 },
+      { lower_bound: 100_001, upper_bound: 200_000, fee: 450.00 },
+      { lower_bound: 200_001, upper_bound: 300_000, fee: 475.00 },
+      { lower_bound: 300_001, upper_bound: 400_000, fee: 525.00 },
+      { lower_bound: 400_001, upper_bound: 500_000, fee: 575.00 },
+      { lower_bound: 500_001, upper_bound: 600_000, fee: 625.00 },
+      { lower_bound: 600_001, upper_bound: 700_000, fee: 700.00 },
+      { lower_bound: 700_001, fee: 750.00 }
+    ],
+    london:
     [
       { lower_bound: 0, upper_bound: 100_000, fee: 675.00 },
       { lower_bound: 100_001, upper_bound: 200_000, fee: 700.00 },
@@ -49,17 +54,12 @@ ConveyancingCalculator.configure do |config|
       { lower_bound: 150_000_0, upper_bound: 200_000_0, fee: 1500.00 },
       { lower_bound: 200_000_1, upper_bound: 250_000_0, fee: 1650.00 },
       { lower_bound: 250_000_0, fee: 1650.00 }
-    ] }
+    ]
+  }
 
   # Used within the models
-  config.additional_costs = { outside_london: [
-    { shared_ownership_scheme: 150 },
-    { leasehold_house: 50 },
-    { leasehold_apartment: 150 },
-    { help_to_buy_scheme: 150 },
-    { help_to_buy_isa: 50 },
-    { deed_of_trust: 150 }
-  ], london:
+  config.additional_costs = {
+    outside_london:
     [
       { shared_ownership_scheme: 150 },
       { leasehold_house: 50 },
@@ -67,13 +67,26 @@ ConveyancingCalculator.configure do |config|
       { help_to_buy_scheme: 150 },
       { help_to_buy_isa: 50 },
       { deed_of_trust: 150 }
-    ] }
+    ],
+    london:
+    [
+      { shared_ownership_scheme: 150 },
+      { leasehold_house: 50 },
+      { leasehold_apartment: 150 },
+      { help_to_buy_scheme: 150 },
+      { help_to_buy_isa: 50 },
+      { deed_of_trust: 150 }
+    ]
+  }
 
   # Static in the code
-  config.additional_static_costs = { outside_london: [
-    { premium_search: 270.83 }
-  ], london:
+  config.additional_static_costs = {
+    outside_london: [
+      { premium_search: 270.83 }
+    ],
+    london:
     [
       { premium_search: 270.83 }
-    ] }
+    ]
+  }
 end
