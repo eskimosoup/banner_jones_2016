4ba3b92264173d05b05eba76f458a1a7ef79fa90
Paul <pauloptimised@users.noreply.github.com>
2018-05-15 17:11:16 +0100

    Disable development cache

--- public/assets/components/team-members/index-profile-card-4b3971807db09846f8500545137093cb321cac44bf6ceb0d1ac758422232edca.js
+++
@@ -1,1 +0,0 @@
-function teamMemberEqualHeightProfileCard(){Modernizr.mq("only screen and (min-width: 1024px)")&&$(".index-team-member-profile-content").matchHeight()}$(document).ready(function(){teamMemberEqualHeightProfileCard()});
