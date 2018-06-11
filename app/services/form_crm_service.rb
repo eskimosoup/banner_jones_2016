6c1ff0a823bcb48799cac8b634f585ad254b194e
Paul <pauloptimised@users.noreply.github.com>
2018-06-11 13:38:07 +0100

    Add subject

--- app/services/form_crm_service.rb
+++ app/services/form_crm_service.rb
@@ -10,6 +10,10 @@ class FormCrmService
   def call
     check_visitor_key
     Post.new(map_postable_fields.merge(map_combined_fields), referer).call
+  rescue CdVisitorKeyMissing => e
+    CustomLogger
+      .new('click_dimensions')
+      .fatal([Time.zone.now, '-', e.message].join(' '))
   end
 
   private
@@ -18,10 +22,6 @@ class FormCrmService
 
   def check_visitor_key
     raise CdVisitorKeyMissing, 'Visitor key missing' if visitor_key.blank?
-  rescue CdVisitorKeyMissing => e
-    CustomLogger
-      .new('click_dimensions')
-      .fatal([Time.zone.now, '-', e.message].join(' '))
   end
 
   def map_postable_fields
@@ -32,7 +32,7 @@ class FormCrmService
       'contact[surname]' => object.try(:surname),
       'contact[preferred_contact_method]' => object.try(:preferred_contact_method),
       'contact[how_heard]' => object.try(:how_heard),
-      'contact[subject]' => object.class.name.humanize,
+      'contact[subject]' => object.class.model_name.human.titleize,
       'cd_visitorkey' => visitor_key
     }
   end
